5677a8bf7c1b3b50ec819dcd849522c46e55ae96
Zafar <zafar.a.ali2@gmail.com>
2019-04-20 16:23:32 -0400

    Initial commit
    - ListView in MainActivity
    - Google Map Activity with default marker in Australia (will change later)

---
+++ app/src/main/java/com/zafar/memorableplaces/MainActivity.java
@@ -0,0 +1,41 @@
+package com.zafar.memorableplaces;
+
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.listView);
+
+        ArrayList<String> places = new ArrayList<String>();
+        places.add("Add a new memorable place...");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, places);
+
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+                intent.putExtra("placeID", position);
+
+                startActivity(intent);
+            }
+        });
+    }
+}
