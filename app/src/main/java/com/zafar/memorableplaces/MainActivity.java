950cce10353118a70dd859c22f5662fa8f158b94
Zafar <zafar.a.ali2@gmail.com>
2019-04-20 17:25:02 -0400

    Marker Put On User Location

--- app/src/main/java/com/zafar/memorableplaces/MainActivity.java
+++ app/src/main/java/com/zafar/memorableplaces/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
                 intent.putExtra("placeID", position);
 
                 startActivity(intent);
--- app/src/main/java/com/zafar/memorableplaces/MapsActivity.java
+++ app/src/main/java/com/zafar/memorableplaces/MapsActivity.java
@@ -1,8 +1,17 @@
 package com.zafar.memorableplaces;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -15,6 +24,29 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    LocationManager locationManager;
+    LocationListener locationListener;
+
+    public void zoomMapLocation(Location location, String title) {
+        if (location != null) {
+            LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
+            mMap.clear();
+            mMap.addMarker(new MarkerOptions().position(userLocation).title(title));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 13));
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+                Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                zoomMapLocation(lastKnownLocation, "Your Location");
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,11 +73,42 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         Intent intent = getIntent();
-        Toast.makeText(getApplicationContext(), Integer.toString(intent.getIntExtra("placeID", 0)), Toast.LENGTH_SHORT ).show();
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // User wants to add a new place
+      if (intent.getIntExtra("placeID", 0) == 0){
+          // Zoom in on user location
+          locationManager = (LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
+          locationListener = new LocationListener() {
+              @Override
+              public void onLocationChanged(Location location) {
+                  zoomMapLocation(location, "Your Location");
+              }
+
+              @Override
+              public void onStatusChanged(String provider, int status, Bundle extras) {
+
+              }
+
+              @Override
+              public void onProviderEnabled(String provider) {
+
+              }
+
+              @Override
+              public void onProviderDisabled(String provider) {
+
+              }
+          };
+
+          if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+              locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+                Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                zoomMapLocation(lastKnownLocation, "Your Location");
+          }else{
+              ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
+          }
+        }
+
     }
 }
