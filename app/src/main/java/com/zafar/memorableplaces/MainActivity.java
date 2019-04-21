ab3fead82a020d833931dbf81d8db2eece6812f8
Zafar <zafar.a.ali2@gmail.com>
2019-04-20 22:01:08 -0400

    Place Deletion From ListView

--- app/src/main/java/com/zafar/memorableplaces/MainActivity.java
+++ app/src/main/java/com/zafar/memorableplaces/MainActivity.java
@@ -1,11 +1,17 @@
 package com.zafar.memorableplaces;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -23,12 +29,13 @@ public class MainActivity extends AppCompatActivity {
     static ArrayAdapter arrayAdapter;
     static SharedPreferences sharedPreferences;
     static HashSet<String> set;
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView listView = findViewById(R.id.listView);
+        listView = findViewById(R.id.listView);
 
 
 
@@ -73,7 +80,44 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                final int itemToDelete = position;
+
+                new AlertDialog.Builder(MainActivity.this)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setTitle("Are you sure?")
+                        .setMessage("Do you want to delete this place?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                places.remove(itemToDelete);
+                                arrayAdapter.notifyDataSetChanged();
+
+                                HashSet<String> set = new HashSet<>(MainActivity.places);
+                                sharedPreferences.edit().putStringSet("placesList", set).apply();
+                                final Toast toast = Toast.makeText(getApplicationContext(), "Place Deleted",
+                                        Toast.LENGTH_SHORT);
+                                toast.show();
+                                Handler handler = new Handler();
+                                handler.postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        toast.cancel();
+                                    }
+                                }, 800);
+                            }
+                        })
+                        .setNegativeButton("No",null)
+                        .show();
+
+                return true;
+            }
+        });
     }
+
 
 
 }
